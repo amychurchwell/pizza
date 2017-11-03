b34e3313cc2cd407b4810e9a1b7b3c5443c606d3
Amy Churchwell <amy.churchwell@gmail.com>
2017-11-03 08:40:21 -0700

    create basic bootstrap layout and form

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
+//REMEMBER GH-PAGES//
 
 //PIZA CONSTRUCTOR: TOPPINGS AND SIZE
+function Pizza(toppings, size){
+  this.toppings = toppings;
+  this.size = size;
+}
 
 //CHOOSE TOPPINGS AND SIZE
 
 //PROTOTYPE METHOD FOR COST DEPENDING ON SELECTIONS
+Pizza.prototype.pizzaCost = function(){
+    var result =
+  };
