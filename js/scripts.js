1d8c667ce6102a2c7aa9eaa1f57b62ce2ce15b08
Amy Churchwell <amy.churchwell@gmail.com>
2017-11-03 13:19:35 -0700

    change form layout add CSS and second pizza button

--- js/scripts.js
+++ js/scripts.js
@@ -23,13 +23,16 @@ var newPizza = new Pizza();
 
 $(document).ready(function(){
 
-
+  $('#addPizza').click(function(){
+    alert('works');
+});
 
   $('#form').submit(function(event){
     event.preventDefault();
 
     var address = $("input#inputAddress").val();
     var zip = $("input#inputZip").val();
+    var name = $("input#inputName").val();
 
     var size = $("input:radio[name=size]:checked").val();
     newPizza.size = size;
@@ -42,7 +45,8 @@ $(document).ready(function(){
     $('#toppings').text(newPizza.toppings);
     $('#size').text(newPizza.size);
     $('#cost').text("$" + newPizza.pizzaCost());
-    $('#delivery').text(address + " Portland, " + zip);
+    $('#delivery').text(name + " " + address + " Portland, " + zip);
+    $('#name').text(name);
 
     //pop up display//
     $('.info').show();
