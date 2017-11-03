7c65d296cd00262451d08d5fcc04bd66a711a16e
Amy Churchwell <amy.churchwell@gmail.com>
2017-11-03 09:12:37 -0700

    add checkbox form functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,30 @@
 
 //PIZA CONSTRUCTOR: TOPPINGS AND SIZE
 function Pizza(toppings, size){
-  this.toppings = toppings;
+  this.toppings = [];
   this.size = size;
 }
 
 //CHOOSE TOPPINGS AND SIZE
 
 //PROTOTYPE METHOD FOR COST DEPENDING ON SELECTIONS
-Pizza.prototype.pizzaCost = function(){
-    var result =
-  };
+// Pizza.prototype.pizzaCost = function(){
+//     var result =
+//   };
+
+$(document).ready(function(){
+
+  var newPizza = new Pizza();
+
+  $('#form').submit(function(event){
+    event.preventDefault();
+
+    $("input:checkbox[name=toppings-input]:checked").each(function(){
+      var yum = $(this).val();
+
+    newPizza.toppings.push(yum)
+    $('#results').text(newPizza.toppings);
+
+    });
+  });
+});
